3a07aa2a9f3462085e914a5d22f6bfed2a204e68
Bek023 <tolmasivich.mansur@gmail.com>
2025-04-15 14:04:13 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,31 @@ import Explore from './Page/Explore'
 import Header from './components/Header'
 import Side_menu from './components/Side_menu'
 import Explorer_one from './components/Explorer_one'
+import { ConfigProvider } from 'antd'
 function App() {
 
 
   return (
     <>
-      <Header />
-      <div className='app_block'>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#52c41a",
+            colorInfo: "#52c41a",
+          },
+        }}
+      >
+        <Header />
+        <div className='app_block'>
+          <Side_menu />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Explore' element={<Explore />} />
+            <Route path='/explore/:id' element={<Explorer_one />} />
+          </Routes>
+        </div>
+      </ConfigProvider>
 
-        <Side_menu />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Explore' element={<Explore />} />
-          <Route path='/explore/:id' element={<Explorer_one />} />
-
-        </Routes>
-      </div>
     </>
   )
 }
